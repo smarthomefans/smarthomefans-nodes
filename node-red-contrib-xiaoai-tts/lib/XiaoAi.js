cece771cb67efedc8242ec0bab41d4c5ad51b190
孙善明 <sunshanming@zhuojianchina.com>
2019-08-30 14:18:42 +0800

    完整主体小米tts

---
+++ node-red-contrib-xiaoai-tts/lib/XiaoAi.js
@@ -0,0 +1,86 @@
+const XiaoAiApi = require('xiaoai-tts')
+const fs = require('fs')
+const util = require('util')
+const os = require('os')
+const readAsync = util.promisify(fs.readFile)
+const mkdirp = require('mkdirp')
+
+class XiaoAi {
+
+    constructor(node, config) {
+        this.node = node
+        this.config = config
+    }
+
+    // 登录
+    getSession() {
+
+        return new Promise(async (resolve, reject) => {
+
+            try {
+                const parentDir = `${os.homedir()}/.node-red/xiaoai`
+                // 创建存储目录
+                mkdirp.sync(parentDir)
+                const path = `${parentDir}/${this.config.username}`
+                let jsonData
+                try {
+                    const data = await readAsync(path)
+                    jsonData = JSON.parse(data)
+                } catch (error) {
+                    jsonData = null
+                }
+
+                if (!jsonData || !jsonData.serviceToken || !jsonData.userId) {
+                    this.client = new XiaoAiApi(this.config.username, this.config.password)
+                    jsonData = await this.client.connect()
+                    fs.writeFileSync(path, JSON.stringify(jsonData))
+                }else {
+                    const {userId, serviceToken} = jsonData
+                    this.client = new XiaoAiApi({userId, serviceToken})
+                }
+                
+                resolve(jsonData)
+
+            } catch (err) {
+                reject(err)
+            }
+        })
+    }
+
+    tts(msg, deviceId) {
+        return new Promise(async (resolve, reject) => {
+
+            try {
+                if (!this.client) {
+                    await this.getSession()
+                }
+    
+                const data =  await this.client.say(msg, deviceId)
+                resolve(data)
+            } catch (error) {
+                reject(error)
+            }
+            
+        })
+    }
+
+    deviceList() {
+        return new Promise(async (resolve, reject) => {
+
+            try {
+                if (!this.client) {
+                    await this.getSession()
+                }
+    
+                const data =  await this.client.getDevice()
+                resolve(data)
+            } catch (error) {
+                reject(error)
+            }
+            
+        })
+    }
+
+}
+
+module.exports = XiaoAi
--- node-red-contrib-xiaoai-tts/xiaoai-tts.js
+++ node-red-contrib-xiaoai-tts/xiaoai-tts.js
@@ -1,33 +1,22 @@
-module.exports = RED => {
-  // tts
-  RED.nodes.registerType('xiaoai-tts', class {
-    constructor (config) {
-      const node = this
-      RED.nodes.createNode(node, config)
-      node.on('input', async data => {
-        try {
-          // 合并值,未细想
-          for (const key in config) { if (config[key] != '' && config[key] != null) { data[key] = config[key] } }
-          data.payload = data.deviceSerial || data.payload
-        } catch (err) {
-          node.status({ text: err.message, fill: 'red', shape: 'ring' })
-          node.warn(err)
-          data.payload = {}
-          data.error_msg = err.message
-          node.send([null, data])
-        }
-      })
-    }
-  })
+const Xiaoai = require('./lib/XiaoAi')
 
+module.exports = RED => {
+
+  
   // devices list
   RED.nodes.registerType('xiaoai-devices', class {
     constructor (config) {
       const node = this
       RED.nodes.createNode(node, config)
+      
+      const xiaomiConfig = RED.nodes.getNode(config.xiaoai)
+      const xiaoai = new Xiaoai(node, xiaomiConfig)
       node.on('input', async data => {
         try {
-
+          const res = await xiaoai.deviceList()
+          data.res = res
+          node.status({ text: `获取设备列表成功:${data._msgid}` })
+          node.send([data, null])
         } catch (err) {
           node.status({ text: err.message, fill: 'red', shape: 'ring' })
           node.warn(err)
@@ -38,4 +27,61 @@ module.exports = RED => {
       })
     }
   })
+
+
+  // tts
+  RED.nodes.registerType('xiaoai-tts', class {
+    constructor (config) {
+      const node = this
+      RED.nodes.createNode(node, config)
+      const xiaomiConfig = RED.nodes.getNode(config.xiaoai)
+      const xiaoai = new Xiaoai(node, xiaomiConfig)
+      node.msgQueue = [];
+      node.on('input', data => {
+        for (const key in config) { if (config[key] != '' && config[key] != null) { data[key] = config[key] } }
+        data.payload = data.tts || data.payload
+
+        let msgQueue = node.msgQueue;
+        if (msgQueue.push(data) > 1) {
+            // pending write exists
+            return;
+        }
+        try {
+            processQ(msgQueue);
+        }
+        catch (e) {
+            node.msgQueue = [];
+            throw e;
+        }
+      })
+
+      async function processMsg(msg, done) {
+        try {
+
+          const res = await xiaoai.tts(msg.payload, msg.device)
+          msg.res = res
+          node.status({ text: `tts 成功:${msg._msgid}` })
+          node.send([msg, null])
+        } catch (err) {
+          node.status({ text: err.message, fill: 'red', shape: 'ring' })
+          node.warn(err)
+          msg.payload = {}
+          msg.error_msg = err.message
+          node.send([null, msg])
+        }
+        done()
+      }
+
+      function processQ(queue) {
+        var msg = queue[0];
+        processMsg(msg, function() {
+            queue.shift();
+            if (queue.length > 0) {
+                processQ(queue);
+            }
+        });
+      }
+    }
+  })
+
 }
